df834153be641f071f694a0b55ec4b619a9017dd
Дмитрий <boozz@Dmitrijs-iMac.local>
2015-01-10 19:06:22 +0200

    change

---
+++ config/autoload/auth.global.php
@@ -0,0 +1,36 @@
+<?php
+return array(
+	'zfcuser' => array(
+        // telling ZfcUser to use our own class
+        'user_entity_class'       => 'Admin\Entity\User',
+        // telling ZfcUserDoctrineORM to skip the entities it defines
+        'enable_default_entities' => false,
+    ),
+
+    'bjyauthorize' => array(
+        // Using the authentication identity provider, which basically reads the roles from the auth service's identity
+        'default_role' => 'guest',
+        'authenticated_role' => 'guest',
+        'identity_provider' => 'BjyAuthorize\Provider\Identity\AuthenticationIdentityProvider',
+
+        'role_providers'        => array(
+            // using an object repository (entity repository) to load all roles into our ACL
+            'BjyAuthorize\Provider\Role\ObjectRepositoryProvider' => array(
+                'object_manager'    => 'doctrine.entitymanager.orm_default',
+                'role_entity_class' => 'Admin\Entity\Role',
+            ),
+        ),
+
+
+        'guards' => array(
+           'BjyAuthorize\Guard\Route' => array(
+                array('route' => 'home', 'roles' => array('guest')),
+                array('route' => 'zfcuser', 'roles' => array('user')),
+                array('route' => 'zfcuser/logout', 'roles' => array('user')),
+                array('route' => 'zfcuser/login', 'roles' => array('guest')),
+                array('route' => 'zfcuser/register', 'roles' => array('guest')),
+                // array('route' => 'admin', 'roles' => array('admin'))
+            ),
+        ),
+    ),
+	);
